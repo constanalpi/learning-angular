68f6e82da04302c222352f2f95aa1c0edc1bfd23
constanalpi <uo232469@uniovi.es>
2017-09-28 18:22:22 +0200

    finished angular-forms unit

--- angular-forms/src/app/app.module.ts
+++ angular-forms/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { HeroFormComponent } from './hero-form.component';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule
   ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    HeroFormComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
